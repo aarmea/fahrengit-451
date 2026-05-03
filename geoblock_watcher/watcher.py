efe5487c505787228c043a8db9d26b251919c9bc
Albert Armea <github@albertarmea.com>
2026-05-03 02:32:53 +0000

    Increase HTTP request size limit
    This fixes Docker image push jobs, like this one
    https://git.armeafamily.com/albert/shepherd-launcher/actions/runs/38/jobs/0/attempt/1

--- geoblock_watcher/watcher.py
+++ geoblock_watcher/watcher.py
@@ -53,7 +53,6 @@ PROXY_DIRECTIVES = """\
     proxy_set_header   X-Real-IP         $remote_addr;
     proxy_set_header   X-Forwarded-For   $proxy_add_x_forwarded_for;
     proxy_set_header   X-Forwarded-Proto $scheme;
-    client_max_body_size 512m;
     proxy_request_buffering off;
     proxy_buffering         off;
     proxy_read_timeout      600s;
